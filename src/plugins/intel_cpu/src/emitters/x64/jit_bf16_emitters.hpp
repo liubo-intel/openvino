222f7e431a87118127b9b872047170075fefcef0
liubo-intel <bo4.liu@intel.com>
2023-08-28 23:25:19 -0700

    fix bf16_emitters AVX2 issue for int8 models ACC.[WA] remove this fix when PR-19358 merged

--- src/plugins/intel_cpu/src/emitters/x64/jit_bf16_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/x64/jit_bf16_emitters.hpp
@@ -75,6 +75,8 @@ private:
             h->uni_vpackusdw(aux, aux, aux);
 
             if (host_isa_ == dnnl::impl::cpu::x64::cpu_isa_t::avx2) {
+                // [WA] remove this fix when PR-19358 merged
+                h->vpermq(Ymm(aux.getIdx()), Ymm(aux.getIdx()), 0xD8);  // 11 01 10 00
                 h->vextracti128(out, Ymm(aux.getIdx()), 0);
             } else {
                 h->uni_vmovups(out, aux);
