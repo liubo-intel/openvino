539f4d706da8b856e562e6f20e94ca0d7246bbd7
liubo-intel <bo4.liu@intel.com>
2026-04-20 07:10:38 -0400

    add PagedCausalConv1D past_lens support for CPU and GPU

--- src/plugins/intel_gpu/tests/unit/test_cases/paged_causal_conv1d.cpp
+++ src/plugins/intel_gpu/tests/unit/test_cases/paged_causal_conv1d.cpp
@@ -80,8 +80,10 @@ struct paged_causal_conv1d_gpu_test : public ::testing::TestWithParam<paged_caus
             d.past_lens.push_back(seq_past_len);
             d.cache_interval.push_back(seq_interval);
 
-            const int32_t write_slots = std::max<int32_t>(1, (seq_tokens + seq_interval - 1) / seq_interval);
-            const int32_t required_slots = 1 + write_slots;
+            const int32_t prev_nums = seq_past_len % seq_interval;
+            const int32_t total_cached = prev_nums + seq_tokens;
+            const int32_t max_slot = 1 + (total_cached - 1) / seq_interval;
+            const int32_t required_slots = 1 + std::max<int32_t>(1, max_slot);
             for (int32_t i = 0; i < required_slots; i++) {
                 d.block_indices.push_back(total_blocks + i);
             }
@@ -128,6 +130,9 @@ struct paged_causal_conv1d_gpu_test : public ::testing::TestWithParam<paged_caus
                 continue;
             }
 
+            const int32_t seq_interval = cache_interval[seq];
+            const int32_t prev_nums = (seq_interval > 0) ? (past_lens[seq] % seq_interval) : 0;
+            const int32_t seq_tokens = token_end - token_begin;
             const int32_t read_physical_block = block_indices[blk_begin];
 
             for (int32_t h = 0; h < hidden_size; h++) {
@@ -138,7 +143,7 @@ struct paged_causal_conv1d_gpu_test : public ::testing::TestWithParam<paged_caus
 
                 const float bias_val = conv_bias.empty() ? 0.0f : static_cast<float>(conv_bias[h]);
 
-                for (int32_t t = 0; t < token_end - token_begin; t++) {
+                for (int32_t t = 0; t < seq_tokens; t++) {
                     for (int32_t k = 0; k + 1 < kernel_size; k++) {
                         state[k] = state[k + 1];
                     }
@@ -154,33 +159,18 @@ struct paged_causal_conv1d_gpu_test : public ::testing::TestWithParam<paged_caus
 
                     output_embeds[token_idx * hidden_size + h] = to_data_type<T>(sum);
 
-                    const int32_t interval = cache_interval[seq];
-                    if (interval > 0) {
-                        const int32_t processed_tokens = t + 1;
-                        if ((processed_tokens % interval) == 0) {
-                            const int32_t logical_block = (processed_tokens + interval - 1) / interval;
-                            if (logical_block >= 1 && logical_block < block_span) {
-                                const int32_t physical_block = block_indices[blk_begin + logical_block];
-                                for (int32_t k = 0; k < kernel_size; k++) {
-                                    conv_state_table[state_off(physical_block, h, k)] = to_data_type<T>(state[k]);
-                                }
+                    const int32_t cached_tokens = prev_nums + (t + 1);
+                    const bool interval_hit = (seq_interval > 0) && ((cached_tokens % seq_interval) == 0);
+                    const bool is_last_token = (t == seq_tokens - 1);
+                    if (interval_hit || is_last_token) {
+                        const int32_t slot = (seq_interval > 0) ? (1 + (cached_tokens - 1) / seq_interval) : 1;
+                        if (slot >= 1 && slot < block_span) {
+                            const int32_t physical_block = block_indices[blk_begin + slot];
+                            for (int32_t k = 0; k < kernel_size; k++) {
+                                conv_state_table[state_off(physical_block, h, k)] = to_data_type<T>(state[k]);
                             }
                         }
                     }
-                }
-
-                int32_t final_logical_block = 1;
-                const int32_t interval = cache_interval[seq];
-                const int32_t seq_tokens = token_end - token_begin;
-                if (interval > 0) {
-                    final_logical_block = (seq_tokens + interval - 1) / interval;
-                }
-                if (final_logical_block >= block_span) {
-                    final_logical_block = block_span - 1;
-                }
-                const int32_t final_physical_block = block_indices[blk_begin + final_logical_block];
-                for (int32_t k = 0; k < kernel_size; k++) {
-                    conv_state_table[state_off(final_physical_block, h, k)] = to_data_type<T>(state[k]);
                 }
             }
         }
